b55f4cacb8518cd18d76ddb8b19e69c786fde827
Zouhair <darkzouhair@gmail.com>
2024-01-23 14:51:12 -0800

    test

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+class Square:
+    """A class that defines a Square by size"""
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+class Square:
+    """A class that defines a square by size and can compute area"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
