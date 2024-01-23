96d2664bfb9a359644671f7cddd5fecf312e9994
Zouhair <darkzouhair@gmail.com>
2024-01-23 14:59:00 -0800

    test

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Define a class Square."""
-
 class Square:
-    
     """A class that defines a Square by size"""
-    
     def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
