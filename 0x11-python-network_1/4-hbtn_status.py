e3392f60a21e2d9366b028dedc7423524f2a838b
Zouhair <darkzouhair@gmail.com>
2024-04-26 08:50:17 -0700

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-fetches https://alx-intranet.hbtn.io/status
+Same as Task 0 but using request package
 """
 
 import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
+
+if __name__ == "__main__":
+    response = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
